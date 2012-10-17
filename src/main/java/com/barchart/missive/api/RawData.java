11e10da93e717b37b005b5bb425857a65e79bb9e
Gavin Litchfield <gavinm.litchfield@gmail.com>
2012-10-17 10:11:57 -0500

    Added helper methods to RawData

--- src/main/java/com/barchart/missive/api/RawData.java
+++ src/main/java/com/barchart/missive/api/RawData.java
@@ -1,6 +1,7 @@
 package com.barchart.missive.api;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class RawData {
@@ -9,9 +10,22 @@ public class RawData {
 	
 	private final Map<Integer, Object> data;
 	
+	public RawData(final String name) {
+		this.name = name;
+		data = new HashMap<Integer, Object>();
+	}
+	
 	public RawData(final String name, final Map<Integer, Object> data) {
 		this.name = name;
 		this.data = data;
+	}
+	
+	public void put(final int code, final Object val) {
+		data.put(code, val);
+	}
+	
+	public void put(final Map<Integer, Object> data) {
+		data.putAll(data);
 	}
 	
 	public String name() {
