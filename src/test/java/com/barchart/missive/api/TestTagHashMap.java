d369769b91e3d814f003b0c5779775899f912432
Andrei.Pozolotin <Andrei.Pozolotin@gmail.com>
2012-11-20 14:11:19 -0600

    a

--- src/test/java/com/barchart/missive/api/TestTagHashMap.java
+++ src/test/java/com/barchart/missive/api/TestTagHashMap.java
@@ -5,66 +5,69 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.barchart.missive.refactoring.TagHashMap;
 
 public class TestTagHashMap {
-	
+
+	protected final Logger log = LoggerFactory.getLogger(getClass());
+
 	static Instrumentation instrumentation;
-	
-	public static void premain(String args, Instrumentation inst) {
-        instrumentation = inst;
-    }
-	
+
+	public static void premain(final String args, final Instrumentation inst) {
+		instrumentation = inst;
+	}
+
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Test
 	public void test() {
-		
+
 		final int mapSize = 20;
-		final int testGetSize = 1000000;
-		
+		final int testGetSize = 1000 * 1000;
+
 		final Tag<?>[] tags = new Tag<?>[mapSize];
-		
-		for(int i = 0; i < mapSize; i++) {
+
+		for (int i = 0; i < mapSize; i++) {
 			tags[i] = Tag.create(String.valueOf(i), String.class);
 		}
-		
+
 		final TagHashMap testMap = new TagHashMap(tags);
-		
-		for(final Tag tag : tags) {
+
+		for (final Tag tag : tags) {
 			testMap.set(tag, "Test" + tag.getName());
 		}
-		
+
 		/* Warm up */
-		for(int i = 0; i < testGetSize; i++) {
+		for (int i = 0; i < testGetSize; i++) {
 			testMap.get(tags[(int) Math.floor(Math.random() * mapSize)]);
 		}
-		
-		/* Test our map*/
+
+		/* Test our map */
 		long start = System.currentTimeMillis();
-		for(int i = 0; i < testGetSize; i++) {
+		for (int i = 0; i < testGetSize; i++) {
 			testMap.get(tags[(int) Math.floor(Math.random() * mapSize)]);
 		}
-		System.out.println(System.currentTimeMillis() - start);
-		
+		log.info("test our-impl = {}", System.currentTimeMillis() - start);
+
 		/* Build java hashmap version */
 		final Map<Tag<?>, Object> testJavaMap = new HashMap<Tag<?>, Object>();
-		for(final Tag tag : tags) {
+		for (final Tag tag : tags) {
 			testJavaMap.put(tag, tag.getName());
 		}
-		
-		/* Warm up */
-		for(int i = 0; i < testGetSize; i++) {
-			testJavaMap.get(tags[(int) Math.floor(Math.random() * mapSize)]);
-		}
-		
-		/* Test java map*/
-		start = System.currentTimeMillis();
-		for(int i = 0; i < testGetSize; i++) {
-			testJavaMap.get(tags[(int) Math.floor(Math.random() * mapSize)]);
-		}
-		System.out.println(System.currentTimeMillis() - start);
-		
-	}
 
+		/* Warm up */
+		for (int i = 0; i < testGetSize; i++) {
+			testJavaMap.get(tags[(int) Math.floor(Math.random() * mapSize)]);
+		}
+
+		/* Test java map */
+		start = System.currentTimeMillis();
+		for (int i = 0; i < testGetSize; i++) {
+			testJavaMap.get(tags[(int) Math.floor(Math.random() * mapSize)]);
+		}
+		log.info("test java-hash = {}", (System.currentTimeMillis() - start));
+
+	}
 }
