80734554913ac4fa1db48ba44df7c6b4a8876545
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2012-12-19 08:24:01 -0600

    exceptions

--- src/main/java/com/barchart/missive/fast/FastTagMap.java
+++ src/main/java/com/barchart/missive/fast/FastTagMap.java
@@ -7,6 +7,7 @@
  */
 package com.barchart.missive.fast;
 
+import com.barchart.missive.core.MissiveException;
 import com.barchart.missive.core.SafeTagMap;
 import com.barchart.missive.core.Tag;
 import com.barchart.missive.core.TagMap;
@@ -14,92 +15,93 @@ import com.barchart.missive.core.TagMap;
 /**
  * 
  * @author Gavin M Litchfield
- *
+ * 
  */
 public class FastTagMap extends FastSafeTagMap implements TagMap {
 
 	protected FastTagMap() {
-		
+
 	}
-	
+
 	public FastTagMap(final Tag<?>[] tagz) {
 		super(tagz);
 	}
-	
+
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
-	public void putRaw(final Tag newTag, final Object newValue) {
+	public void putRaw(final Tag newTag, final Object newValue)
+			throws MissiveException {
 		put(newTag, newTag.cast(newValue));
 	}
-	
+
 	@Override
-	public <V> void put(final Tag<V> newTag, V newValue) {
-		
-		if(containsTag(newTag)) {
+	public <V> void put(final Tag<V> newTag, final V newValue) {
+
+		if (containsTag(newTag)) {
 			set(newTag, newValue);
 		} else {
-			
-			Tag<?>[] temp = new Tag<?>[tags.length + 1];
+
+			final Tag<?>[] temp = new Tag<?>[tags.length + 1];
 			System.arraycopy(tags, 0, temp, 0, tags.length);
 			tags = temp;
-			
-			Object[] tempValues = new Object[tags.length];
+
+			final Object[] tempValues = new Object[tags.length];
 			System.arraycopy(values, 0, tempValues, 0, values.length);
 			values = tempValues;
-			
+
 			tagsByIndex[newTag.index()] = newTag;
-			indexLookup[newTag.index()] = values.length-1;
+			indexLookup[newTag.index()] = values.length - 1;
 			values[indexLookup[newTag.index()]] = newValue;
-			
+
 			tags[tags.length - 1] = newTag;
-			
+
 			size++;
-			
+
 		}
 	}
-	
+
 	@Override
-	@SuppressWarnings({ "rawtypes"})
+	@SuppressWarnings({ "rawtypes" })
 	public Object remove(final Tag oldTag) {
-		
-		if(!containsTag(oldTag)) {
+
+		if (!containsTag(oldTag)) {
 			return null;
 		}
-		
+
 		final Object oldValue = values[indexLookup[oldTag.index()]];
-		
+
 		values[indexLookup[oldTag.index()]] = null;
-		
-		//This may cause problems
-		for(int i = 0; i < tags.length; i++) {
-			if(tags[i] == oldTag) {
+
+		// This may cause problems
+		for (int i = 0; i < tags.length; i++) {
+			if (tags[i] == oldTag) {
 				tags[i] = null;
 				break;
 			}
 		}
-		
+
 		tagsByIndex[oldTag.index()] = null;
-		
+
 		size--;
-		
+
 		return oldValue;
 	}
 
 	@Override
-	public boolean isSupersetOf(SafeTagMap map) {
-		for(final Tag<?> thatTag : map.getTags()) {
+	public boolean isSupersetOf(final SafeTagMap map) {
+		for (final Tag<?> thatTag : map.getTags()) {
 			boolean tagCheck = false;
-			for(final Tag<?> thisTag : tags) {
-				if(thatTag==thisTag) {
+			for (final Tag<?> thisTag : tags) {
+				if (thatTag == thisTag) {
 					tagCheck = true;
 				}
 			}
-			if(!tagCheck) {
+			if (!tagCheck) {
 				return false;
 			}
 		}
-		
+
 		return true;
 	}
-	
+
 }
