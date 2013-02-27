3dc2e96ff80ccc0c6390a030158a8b7c1891fe51
glitchfield <gavin.litchfield@barchart.com>
2013-02-27 13:03:35 -0600

    test tweaks

--- src/test/java/com/barchart/missive/core/MissiveMemoryTest2.java
+++ src/test/java/com/barchart/missive/core/MissiveMemoryTest2.java
@@ -6,13 +6,10 @@ import java.lang.management.MemoryUsage;
 import java.util.HashMap;
 import java.util.Map;
 
-/*
- * Results 100,000 instances = 35MB
- * Results 1,000,000 instances = 204MB
- * Results 10,000,000 instances = 1,832 MB
- */
-public class MissiveMemoryTest {
+import com.barchart.missive.core.MissiveMemoryTest.TestMissive;
 
+public class MissiveMemoryTest2 {
+	
 	public static final int TEST_SIZE = 1 * 1000 * 1000;
 
 	public static final Tag<Long> TAG1 = Tag.create(Long.class);
@@ -38,14 +35,14 @@ public class MissiveMemoryTest {
 
 	public static final Tag<?>[] TAGS = Tag.collectTop(MissiveMemoryTest.class);
 
-	public static class TestMissive extends Missive {
+	public static class TestMissive extends MissiveSafe {
 
 		static {
 			install(TAGS);
 		}
 
 	}
-
+	
 	public static final Map<Integer, Missive> map = new HashMap<Integer, Missive>();
 
 	static final void doGC(final int count) throws Exception {
@@ -55,7 +52,7 @@ public class MissiveMemoryTest {
 			Thread.sleep(1 * 1000);
 		}
 	}
-
+	
 	public static void main(final String[] args) throws Exception {
 
 		final MemoryMXBean memBean = ManagementFactory.getMemoryMXBean();
@@ -68,7 +65,7 @@ public class MissiveMemoryTest {
 		final int batch = 100 * 1000;
 
 		for (int i = 0; i < TEST_SIZE; i++) {
-			map.put(i, Missive.build(TestMissive.class));
+			map.put(i, makeNewTestMissive());
 			if (i % batch == 0) {
 				System.out.println(String.format("Map : %,d", i));
 			}
@@ -102,6 +99,18 @@ public class MissiveMemoryTest {
 			
 		}
 
+	}
+	
+	public static Missive makeNewTestMissive() {
+		
+		MissiveSafe m = Missive.build(TestMissive.class);
+		
+		for(Tag t : TAGS) {
+			m.set(t, new Long((long) Math.random() * 100));
+		}
+		
+		return m;
+		
 	}
 
 }
